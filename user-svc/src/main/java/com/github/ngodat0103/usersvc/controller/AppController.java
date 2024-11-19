813d89941f7b8723821c73958e25d8d3939255e5
ngodat0103 <ngovuminhdat@gmail.com>
2024-11-19 19:26:02 +0700

    feat(controller): add AppController to expose application version - Introduced `AppController` to provide an endpoint for version info. - Updated `application.yaml` to utilize the new `APP_VERSION` variable. - Cleaned up `compose.yaml` by defining `APP_VERSION` in the environment.

---
+++ user-svc/src/main/java/com/github/ngodat0103/usersvc/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.github.ngodat0103.usersvc.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@AllArgsConstructor
+public class AppController {
+    @GetMapping(value = "/version",produces = "text/plain")
+    Mono<String> getVersion() {
+        return Mono.just(System.getenv("APP_VERSION"));
+    }
+}
