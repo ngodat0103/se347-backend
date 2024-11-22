089a0174096fe9c24cebc5fb4d7f858d381d55fc
ngodat0103 <ngovuminhdat@gmail.com>
2024-11-22 16:49:27 +0700

    fix(AuditService): Refactor to use CommandLineRunner for async execution Updated AuditService to implement CommandLineRunner instead of ApplicationListener. This change enables the asynchronous execution of the listenToChanges method

--- user-svc/src/main/java/com/github/ngodat0103/usersvc/service/AuditService.java
+++ user-svc/src/main/java/com/github/ngodat0103/usersvc/service/AuditService.java
@@ -3,20 +3,32 @@ package com.github.ngodat0103.usersvc.service;
 import com.github.ngodat0103.usersvc.persistence.document.Account;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import reactor.core.scheduler.Schedulers;
 
 @Service
-@AllArgsConstructor
 @Slf4j
-public class AuditService implements ApplicationListener<ApplicationReadyEvent> {
-  private ReactiveMongoTemplate reactiveMongoTemplate;
-  @Override
-  public void onApplicationEvent(ApplicationReadyEvent event) {
-    listenToChanges();
+@Configuration
+public class AuditService implements CommandLineRunner {
+  private final ReactiveMongoTemplate reactiveMongoTemplate;
+    private final TaskExecutor taskExecutor;
+
+  public AuditService(ReactiveMongoTemplate reactiveMongoTemplate) {
+    this.reactiveMongoTemplate = reactiveMongoTemplate;
+    this.taskExecutor = new SimpleAsyncTaskExecutor();
   }
 
   public void listenToChanges() {
@@ -33,6 +45,11 @@ public class AuditService implements ApplicationListener<ApplicationReadyEvent> 
               System.out.println("Change event: " + change);
               int stop = 0;
             })
-        .blockLast();
+            .blockLast();
   }
+
+    @Override
+    public void run(String... args) throws Exception {
+        taskExecutor.execute(this::listenToChanges);
+    }
 }
