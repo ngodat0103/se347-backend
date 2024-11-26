8687bb88631dedd3d960ea746aa93a401a49e4d6
ngodat0103 <ngovuminhdat@gmail.com>
2024-11-26 08:02:23 +0700

    🔧 fix(RedisConfiguration): update Redis key serialization to use StringRedisSerializer for better compatibility
    🔒 fix(dev-pipeline): correct MongoDB password secret path in GitHub Actions workflow configuration

--- user-svc/src/main/java/com/github/ngodat0103/usersvc/config/RedisConfiguration.java
+++ user-svc/src/main/java/com/github/ngodat0103/usersvc/config/RedisConfiguration.java
@@ -9,18 +9,19 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
 
     @Bean
     ReactiveRedisTemplate<String, TopicRegisteredUser> reactiveRedisTemplate(ReactiveRedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
-
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         Jackson2JsonRedisSerializer<TopicRegisteredUser> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, TopicRegisteredUser.class);
         RedisSerializationContext<String, TopicRegisteredUser> serializationContext = RedisSerializationContext
                 .<String, TopicRegisteredUser>newSerializationContext()
-                .key(RedisSerializer.string())
-                .hashKey(RedisSerializer.string())
+                .key(stringRedisSerializer)
+                .hashKey(stringRedisSerializer)
                 .hashValue(jackson2JsonRedisSerializer)
                 .value(jackson2JsonRedisSerializer)
                 .build();
