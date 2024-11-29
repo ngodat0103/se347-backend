94594a567f1762d6af8d02e40cb99461d93a2b57
ngodat0103 <ngovuminhdat@gmail.com>
2024-11-29 19:17:36 +0700

    🔧 chore(.github/workflows/dev-java-ci-template.yaml): update Snyk check and report handling in CI workflow

--- user-svc/src/main/java/com/github/ngodat0103/usersvc/UserServiceApplication.java
+++ user-svc/src/main/java/com/github/ngodat0103/usersvc/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceApplication {
-  // testfdsf
+  // testfd
   public static void main(String[] args) {
     SpringApplication.run(com.github.ngodat0103.usersvc.UserServiceApplication.class, args);
   }
