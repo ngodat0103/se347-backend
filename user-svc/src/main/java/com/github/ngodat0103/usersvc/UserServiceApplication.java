88cef28ad336754295220eaf00b96a0c04f67ecf
Ngô Vũ Minh Đạt <ngovuminhdat@gmail.com>
2024-12-01 10:17:58 +0700

    Optmize the pipeline (#12)
    * Utilize caching. parallel job
    
    * 🔧 chore(.github/workflows/dev-java-ci-template.yaml): update Snyk check and report handling in CI workflow
    
    * 🔧 fix(.github/workflows): update Snyk and Trivy report paths and conditions for pull request comments

--- user-svc/src/main/java/com/github/ngodat0103/usersvc/UserServiceApplication.java
+++ user-svc/src/main/java/com/github/ngodat0103/usersvc/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceApplication {
-  // test
+  // testfddasfdsfsdffdsf
   public static void main(String[] args) {
     SpringApplication.run(com.github.ngodat0103.usersvc.UserServiceApplication.class, args);
   }
