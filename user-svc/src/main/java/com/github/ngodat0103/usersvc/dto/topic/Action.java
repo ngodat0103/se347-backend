1ccbd4257d5395ab28dceaa8c89490f9f0cdb671
ngodat0103 <ngovuminhdat@gmail.com>
2024-12-14 15:25:43 +0700

    clean()

--- user-svc/src/main/java/com/github/ngodat0103/usersvc/dto/topic/Action.java
+++
@@ -1,9 +0,0 @@
-package com.github.ngodat0103.usersvc.dto.topic;
-
-public enum Action {
-  INSERT,
-  UPDATE,
-  DELETE,
-  REPLACE,
-  RESEND_EMAIL_VERIFICATION,
-}
--- user-svc/src/main/java/com/github/ngodat0103/usersvc/service/kafka/KafkaService.java
+++
@@ -1,7 +0,0 @@
-package com.github.ngodat0103.usersvc.service.kafka;
-
-public interface KafkaService {
-  void sendMessage(String topic, Object key, Object message);
-
-  boolean supports(Object key, Object message);
-}
