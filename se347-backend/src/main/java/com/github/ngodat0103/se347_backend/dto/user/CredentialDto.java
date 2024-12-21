00fe4822c638d103a505013663213bb18bc4869f
ngodat0103 <ngovuminhdat@gmail.com>
2024-12-21 15:52:15 +0700

    ✨ feat(CredentialDto): add validation annotations for email and password fields to enhance user input checks

--- se347-backend/src/main/java/com/github/ngodat0103/se347_backend/dto/user/CredentialDto.java
+++ se347-backend/src/main/java/com/github/ngodat0103/se347_backend/dto/user/CredentialDto.java
@@ -1,11 +1,18 @@
 package com.github.ngodat0103.se347_backend.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class CredentialDto {
+  @Email(message = "Email should be valid")
+  @NotNull(message = "Email should not be null" )
   private String email;
+  @NotNull(message = "Password should not be null")
+  @Min(value = 8, message = "Password should have at least 8 characters")
   private String password;
 }
